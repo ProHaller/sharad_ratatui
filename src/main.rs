0733e3ce94bc07c285259dba47d2c19a390077c6
ProHaller <88140523+ProHaller@users.noreply.github.com>
2025-03-02 20:44:43 +0900

    Auto-update support

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,15 @@ use crate::cleanup::cleanup;
 use crate::error::ShadowrunError;
 use crate::message::{AIMessage, Message, MessageType};
 
+use core::cmp::Ordering;
 use crossterm::{
     event::{Event, KeyEventKind}, // Event handling from crossterm for input events.
     execute,                      // Helper macro to execute terminal commands.
-    terminal::{enable_raw_mode, EnterAlternateScreen, SetSize}, // Terminal manipulation utilities.
+    terminal::{EnterAlternateScreen, SetSize, enable_raw_mode}, // Terminal manipulation utilities.
 };
-use ratatui::{backend::CrosstermBackend, Terminal};
+use ratatui::{Terminal, backend::CrosstermBackend};
+use self_update::backends::github::{ReleaseList, Update};
+use semver::Version;
 use std::panic; // Panic handling for cleanup.
 use std::{io, sync::Arc, time::Duration}; // Standard I/O and concurrency utilities.
 use tokio::sync::mpsc; // Asynchronous message passing channel.
@@ -43,7 +46,7 @@ const MIN_HEIGHT: u16 = 40;
 // Function to ensure the terminal size meets minimum requirements.
 fn ensure_minimum_terminal_size() -> io::Result<()> {
     let (width, height) = crossterm::terminal::size()?; // Get current size of the terminal.
-                                                        // If the current size is less than minimum, resize to the minimum required.
+    // If the current size is less than minimum, resize to the minimum required.
     if width < MIN_WIDTH || height < MIN_HEIGHT {
         execute!(
             io::stdout(),
@@ -56,6 +59,10 @@ fn ensure_minimum_terminal_size() -> io::Result<()> {
 // Entry point for the Tokio runtime.
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let update_result = tokio::task::spawn_blocking(check_for_updates).await?;
+    if let Err(e) = update_result {
+        println!("Failed to check for updates: {}", e);
+    }
     // Set up the terminal in raw mode.
     enable_raw_mode()?;
     let mut stdout = io::stdout();
@@ -249,4 +256,51 @@ async fn run_app(
         }
         last_tick = Instant::now();
     }
+}
+
+fn check_for_updates() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    println!("Checking for updates...");
+
+    let repo_owner = "ProHaller";
+    let repo_name = "sharad_ratatui";
+    let binary_name = "sharad";
+    let current_version = env!("CARGO_PKG_VERSION");
+
+    let releases = ReleaseList::configure()
+        .repo_owner(repo_owner)
+        .repo_name(repo_name)
+        .build()?
+        .fetch()?;
+
+    if let Some(release) = releases.first() {
+        println!("Newest version found: {}", release.version);
+
+        let latest_version = Version::parse(&release.version)?;
+        let current_version = Version::parse(current_version)?;
+
+        match latest_version.cmp(&current_version) {
+            Ordering::Greater => {
+                println!("Updating to new version: {}", release.version);
+                Update::configure()
+                    .repo_owner(repo_owner)
+                    .repo_name(repo_name)
+                    .bin_name(binary_name)
+                    .target(self_update::get_target())
+                    .show_download_progress(true)
+                    .show_output(true)
+                    .bin_install_path(std::env::current_exe()?.parent().unwrap())
+                    .current_version(&current_version.to_string())
+                    .target_version_tag(&release.version)
+                    .build()?
+                    .update()?;
+            }
+            Ordering::Equal => println!("Current version is up to date."),
+            Ordering::Less => println!("You're in the future."),
+        }
+    } else {
+        println!("No new updates found.");
+    }
+
+    println!();
+    Ok(())
 }
